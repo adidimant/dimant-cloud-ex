24acb16b50cdac6dd29c749eeb95c5c62cbd7e40
adidimant <adi.dimant@gmail.com>
2020-09-23 02:31:07 +0300

    Add external lambda function that trigger inner lambda, make order in template.yml buckets

---
+++ src/handlers/inner-handler.js
@@ -0,0 +1,54 @@
+const uuid = require('uuid');
+const {ALERTS} = require("../lib/consts");
+const PUBLIC_BUCKET_NAME = process.env.PUBLIC_BUCKET, PRIVATE_BUCKET_NAME = process.env.PRIVATE_BUCKET;
+const {getBucketObject, putObjectInBucket, deleteBucketObject} = require("../lib/aws");
+const {saveFileInfoInDB} = require("../lib/db");
+
+exports.innerHandler = async (event, context) => {
+    console.log("incomingPDFHandler lambda function was triggered!");
+    try {
+        const fileKey = event.Records[0].s3.object.key;
+        let result;
+
+        const originFileParams = {
+            Bucket: PUBLIC_BUCKET_NAME,
+            Key: fileKey,
+        };
+
+        if(!fileKey.endsWith('.pdf')) {
+            await deleteBucketObject(originFileParams);
+            result = `File: ${fileKey} was deleted due to not-supported format`;
+        } else {
+            const fileId = uuid();
+            const originFile = await getBucketObject(originFileParams);
+            if(!originFile) console.log(`${ALERTS.COULDNT_GET_FILE} - ${fileKey}`);
+            else {
+                const putParams = {
+                    Bucket: PRIVATE_BUCKET_NAME,
+                    Key: fileId,
+                    Body: originFile.Body,
+                    ContentType: originFile.ContentType,
+                    Metadata: originFile.Metadata,
+                    StorageClass: "INTELLIGENT_TIERING"
+                };
+                await putObjectInBucket(putParams);
+
+                const fileInfo = {
+                    key: fileId,
+                    original_key: fileKey,
+                    size: event.Records[0].s3.object.size,
+                    type: originFile.ContentType
+                };
+
+                await saveFileInfoInDB(fileInfo);
+
+                result = `File: ${fileKey} was successfully renamed and moved to the private bucket`;
+            }
+        }
+        console.log(result);
+      return { statusCode: 200, result: result };
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+};
--- src/lib/consts.js
+++ src/lib/consts.js
@@ -1,5 +1,4 @@
-const PUBLIC_BUCKET_NAME = "simpleappbucket";
-const PRIVATE_BUCKET_NAME = "dimant-test-secured";
+const REGION = "us-east-1";
 const FILE_UPLOADS_TABLE = "file_uploads_log";
 const actionContext = {
     client_id: 'dimant-cloud-ex',
@@ -10,4 +9,4 @@ const ALERTS = {
     COULDNT_GET_FILE: "Couldn't get file from public bucket"
 }
 
-module.exports = {PUBLIC_BUCKET_NAME, PRIVATE_BUCKET_NAME, FILE_UPLOADS_TABLE, actionContext, ALERTS};
+module.exports = {REGION, FILE_UPLOADS_TABLE, actionContext, ALERTS};
