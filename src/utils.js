d8aa39f1c39a3a2da201f150a1878c4ffa94cf2b
adidimant <adi.dimant@gmail.com>
2020-09-29 12:02:40 +0300

    Delete former utils module

--- src/utils.js
+++
@@ -1,7 +0,0 @@
-const querystring = require("querystring");
-
-const queryObjectKeyToString = (queryKey) => (
-    Object.keys(querystring.parse(queryKey))[0]
-);
-
-module.exports = {queryObjectKeyToString};
